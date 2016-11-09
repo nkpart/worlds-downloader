4191a32f19b71ab51ecbc5246752e232d59fadf1
Nick Partridge <nkpart@gmail.com>
2016-11-09 10:32:45 +1000

    2016

--- src/Worlds.hs
+++ src/Worlds.hs
@@ -6,6 +6,7 @@ module Worlds
 
 import Control.Lens hiding (children)
 import Data.ByteString.Lazy (ByteString)
+import Debug.Trace
 import Data.Text.Encoding.Error (lenientDecode)
 import Data.Text.Lazy.Encoding (decodeUtf8With)
 import Network.Wreq
@@ -29,28 +30,40 @@ main :: IO ()
 main =
   do [dest] <- getArgs
      createDirectory dest
-     medleys <- toListOf performances <$> get medleyRoot
-     msrs <- toListOf performances <$> get msrRoot
-     print medleys
-     print msrs
-     for_ (medleys <> msrs) $ \(Performance perfName path) ->
-       do T.putStrLn perfName
-          download (bbc <> path ^. from packed) "tmp.flv"
-          extractAudio "tmp.flv" (dest </> perfName ^. from packed <> ".mp4")
-          removeFile "tmp.flv"
+     -- _2014s <- toListOf performances <$> get _2014
+     perfs <- performancesAt _2016medleys <> performancesAt _2016msrs
+     -- medleys <- toListOf performances <$> get medleyRoot
+     -- msrs <- toListOf performances <$> get msrRoot
+     for_ perfs $
+       \(Performance perfName path) ->
+         -- for_ (medleys <> msrs) $ \(Performance perfName path) ->
+         do T.putStrLn perfName
+            download (bbc <> path ^. from packed) "tmp.flv"
+            -- extractAudio "tmp.flv" (dest </> perfName ^. from packed <> ".mp4")
+            renameFile "tmp.flv"
+                       (dest </> perfName ^. from packed <> ".flv")
   where medleyRoot = "http://www.bbc.co.uk/programmes/p02zrvzr"
         msrRoot = "http://www.bbc.co.uk/programmes/p02zr6z7"
+        _2014 = "http://www.bbc.co.uk/programmes/b04dw148/clips"
+        _2016medleys = "http://www.bbc.co.uk/programmes/p044g8w4"
+        _2016msrs = "http://www.bbc.co.uk/programmes/p044g9f5"
         bbc = "http://www.bbc.co.uk"
+
+performancesAt page =
+     toListOf performances <$> get page
 
 performances :: Fold (Response ByteString) Performance
 performances = responseBody . to (decodeUtf8With lenientDecode) . html . links . to table . _Just
   where links = allAttributed (ix "data-linktrack" . only "programmeobjectlink=cliptitle")
+  -- think this is for 2014 only?
+  -- where links = allAttributed (ix "data-linktrack" . only "programmeobjectlink=title")
 
 table :: Element -> Maybe Performance
 table row =
+  traceShow row $
    do let n = row ^. children . traverse . contents
       href <- row ^? attr "href" . _Just
-      return $! Performance n href
+      return $! traceShow n $ Performance n href
 
 download :: String -> FilePath -> IO ()
 download uri fp = callProcess "youtube-dl" ["-o", fp, uri]
